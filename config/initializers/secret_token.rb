be45544dfe08c441c33ffe10c61d3a0428c14e44
erea <ericwilliamrea@gmail.com>
2014-01-18 13:39:47 -0700

    adding all Shopify app files and the initial app social share for twitter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CleanSocial::Application.config.secret_key_base = '7eddbbb73e19d220d93e0eb7f5f16b6a03c49645c15658eaaebfbd3699a857466a6ca6ace5a5ff596b558b3f4d9b32c56365acc39ac103db3cf98183280e8537'
